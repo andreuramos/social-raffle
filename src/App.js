6edf7ed05fe2d748eb30a05f532bb35a976d0546
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2022-07-21 19:43:00 +0200

    fix homepage

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="start-button"
-        >
-          Start
-        </a>
+          <p className="App-title">InstaRaffler</p>
+          <p className="App-description">Description of what this application does with some fancy words that makes people want to click on the START button.</p>
       </header>
+        <main className="App-main">
+            <a
+                className="start-button"
+            >
+                Start
+            </a>
+      </main>
     </div>
   );
 }
